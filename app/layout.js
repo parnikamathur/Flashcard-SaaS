1dea029dbcde86ab9ef3e594b46b9a23672e2966
padmashiny <padmashin1234@gmail.com>
2024-08-23 21:50:38 +0530

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,19 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs"
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Flashcard SaaS",
-  description: "Create flashcards from your text using AI",
-};
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
